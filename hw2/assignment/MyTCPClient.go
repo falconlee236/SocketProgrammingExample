b1b07ac7e799417c29e389040f90e22858aa3765
falconlee236 <falconlee236@cau.ac.kr>
2024-03-20 20:11:01 +0900

    add signal handler in client

--- hw2/assignment/MyTCPClient.go
+++ hw2/assignment/MyTCPClient.go
@@ -7,13 +7,25 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"os/signal"
 	"strings"
 	"time"
 )
 
 func main() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("captured %v, stopping profiler and exiting..\n", sig)
+			fmt.Print("Bye bye~\n")
+			os.Exit(1)
+		}
+	}()
 
 	serverName := "nsl5.cau.ac.kr"
 	serverPort := "20532"
@@ -49,9 +61,11 @@ func main() {
 		recInfo := strings.Split(string(buffer), ":")
 		fmt.Printf("\nReply from server: client IP = %s PORT = %s\n", recInfo[0], recInfo[1])
 	} else if strings.TrimRight(input_option, "\n") == "3" {
-		fmt.Printf("\nReply from server: %s", string(buffer))
+		fmt.Printf("\nReply from server: requests served = %s\n", string(buffer))
 	} else if strings.TrimRight(input_option, "\n") == "4" {
 		fmt.Printf("\nReply from server: run time = %s", string(buffer))
+	} else if strings.TrimRight(input_option, "\n") == "5" {
+		fmt.Print("Bye bye~\n")
 	}
 	fmt.Printf("RTT = %s\n", duration)
 	conn.Close()
