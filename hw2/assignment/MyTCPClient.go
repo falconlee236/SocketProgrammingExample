d141f0aa13f200de0eb259b2fbadb6d47f61cb9f
falconlee236 <falconlee236@cau.ac.kr>
2024-03-20 14:07:25 +0900

    revise serverPort

--- hw2/assignment/MyTCPClient.go
+++ hw2/assignment/MyTCPClient.go
@@ -4,18 +4,24 @@
 
 package main
 
-import ("bufio"; "fmt"; "net"; "os"; "strings")
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
 
 func main() {
 
-    serverName := "nsl5.cau.ac.kr"
-    serverPort := "12000"
+	serverName := "nsl5.cau.ac.kr"
+	serverPort := "20532"
 
-    conn, _:= net.Dial("tcp", serverName+":"+serverPort)
+	conn, _ := net.Dial("tcp", serverName+":"+serverPort)
 
-    localAddr := conn.LocalAddr().(*net.TCPAddr)
-    fmt.Printf("Client is running on port %d\n", localAddr.Port)
-	
+	localAddr := conn.LocalAddr().(*net.TCPAddr)
+	fmt.Printf("Client is running on port %d\n", localAddr.Port)
+
 	fmt.Printf("<Menu>\n")
 	fmt.Printf("1) convert text to UPPER-case\n")
 	fmt.Printf("2) get my IP address and port number\n")
@@ -23,19 +29,19 @@ func main() {
 	fmt.Printf("4) get server running time\n")
 	fmt.Printf("5) exit\n")
 	fmt.Printf("Input option: ")
-    input_option, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	input_option, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	conn.Write([]byte(input_option))
 	fmt.Printf(input_option)
 
-	if strings.TrimRight(input_option, "\n") == "1"{
-    	fmt.Printf("Input lowercase sentence: ")
-    	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-    	conn.Write([]byte(input))
+	if strings.TrimRight(input_option, "\n") == "1" {
+		fmt.Printf("Input lowercase sentence: ")
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		conn.Write([]byte(input))
 
-    	buffer := make([]byte, 1024)
-    	conn.Read(buffer)
-    	fmt.Printf("Reply from server: %s", string(buffer))
+		buffer := make([]byte, 1024)
+		conn.Read(buffer)
+		fmt.Printf("Reply from server: %s", string(buffer))
 
-    	conn.Close()
+		conn.Close()
 	}
 }
--- hw2/assignment/MyTCPServer.go
+++ hw2/assignment/MyTCPServer.go
@@ -4,24 +4,27 @@
 
 package main
 
-import ("bytes"; "fmt"; "net")
+import (
+	"bytes"
+	"fmt"
+	"net"
+)
 
 func main() {
-    serverPort := "12000"
+	serverPort := "20532"
 
-    listener, _:= net.Listen("tcp", ":" + serverPort)
-    fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+	listener, _ := net.Listen("tcp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
 	typebuffer := make([]byte, 1024)
-    buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024)
 
-    for {
-        conn, _:= listener.Accept()
+	for {
+		conn, _ := listener.Accept()
 		t, _ := conn.Read(typebuffer)
-        fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-        count, _ := conn.Read(buffer)
-        conn.Write(bytes.ToUpper(buffer[:count]))
+		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
+		count, _ := conn.Read(buffer)
+		conn.Write(bytes.ToUpper(buffer[:count]))
 		fmt.Printf("Command %s\n", typebuffer[:t])
-        conn.Close()
-    }
+		conn.Close()
+	}
 }
-
