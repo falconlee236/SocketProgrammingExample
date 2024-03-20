ff01b8945beeb874da87e4794f68091475acde25
falconlee236 <falconlee236@cau.ac.kr>
2024-03-20 13:09:41 +0900

    revise MyTCPSErver.go

--- hw2/assignment/MyTCPServer.go
+++ hw2/assignment/MyTCPServer.go
@@ -11,14 +11,16 @@ func main() {
 
     listener, _:= net.Listen("tcp", ":" + serverPort)
     fmt.Printf("Server is ready to receive on port %s\n", serverPort)
-
+	typebuffer := make([]byte, 1024)
     buffer := make([]byte, 1024)
 
     for {
         conn, _:= listener.Accept()
+		t, _ := conn.Read(typebuffer)
         fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
         count, _ := conn.Read(buffer)
         conn.Write(bytes.ToUpper(buffer[:count]))
+		fmt.Printf("Command %s\n", typebuffer[:t])
         conn.Close()
     }
 }
