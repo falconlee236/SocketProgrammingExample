950cc37c1815d988155b84968d00aea52aaec037
falconlee236 <falconlee236@cau.ac.kr>
2024-03-21 10:42:14 +0900

    add example TCP code

--- hw2/example/TestTCPClient.go
+++ hw2/example/TestTCPClient.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	socket, err := net.Dial("tcp", "localhost:18080")
+	if err != nil {
+		log.Fatalf("Failed to connect to server: %v", err)
+		return
+	}
+
+	defer socket.Close()
+	console := bufio.NewReader(os.Stdin)
+	recvBuf := make([]byte, 4096)
+
+	for {
+		fmt.Print("socket> ")
+		text, _ := console.ReadString('\n')
+
+		if number, err := strconv.Atoi(strings.Trim(string(text), string('\n'))); err == nil {
+			fmt.Printf("Echo delayed: %v\n", number)
+		}
+
+		if text == "quit\n" {
+			break
+		}
+		socket.Write([]byte(text))
+
+		readBytes, err := socket.Read(recvBuf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Printf("Server closed the socket: %v", socket.RemoteAddr().String())
+				return
+			}
+
+			fmt.Printf("Failed to receive data: %v", err)
+			return
+		}
+
+		if readBytes > 0 {
+			data := recvBuf[:readBytes]
+			fmt.Println(string(data))
+		} else {
+			fmt.Println("Can you see me?")
+		}
+	}
+}
--- hw2/example/TestTCPServer.go
+++ hw2/example/TestTCPServer.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	sock, err := net.Listen("tcp", ":18080")
+	if err != nil {
+		log.Fatalf("Failed to bind address: %v", err)
+	}
+
+	defer sock.Close()
+
+	for {
+		conn, err := sock.Accept()
+		if err != nil {
+			log.Printf("Failed to accept: %v", err)
+			continue
+		}
+
+		go ClientHandler(conn)
+	}
+}
+
+func ClientHandler(conn net.Conn) {
+	defer conn.Close()
+
+	recvBuf := make([]byte, 4096)
+
+	for {
+
+		readBytes, err := conn.Read(recvBuf)
+		if err != nil {
+			if err == io.EOF {
+				log.Printf("Client closed the socket: %v", conn.RemoteAddr().String())
+				return
+			}
+
+			log.Printf("Failed to receive data: %v", err)
+			return
+		}
+
+		if readBytes > 0 {
+			data := recvBuf[:readBytes]
+
+			if number, err := strconv.Atoi(strings.Trim(string(data), string('\n'))); err == nil {
+				time.Sleep(time.Duration(number) * time.Second)
+			}
+
+			conn.Write(data)
+
+			log.Println(string(data))
+		} else {
+			log.Println("Can you see me?")
+		}
+
+	}
+}
