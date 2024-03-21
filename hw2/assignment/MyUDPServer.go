6c420b137cfc6d89e6750c0dd2ceb40435c18e87
falconlee236 <falconlee236@cau.ac.kr>
2024-03-21 19:06:59 +0900

    inside pconn statement in function

--- hw2/assignment/MyUDPServer.go
+++ hw2/assignment/MyUDPServer.go
@@ -27,23 +27,20 @@ func main() {
 
 	start := time.Now()
 	reqNum := 0
-	serverPort := "20532"
-
-	pconn, _ := net.ListenPacket("udp", ":"+serverPort)
-	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
-
-	go UDPClientHandler(pconn, reqNum, start)
+	go UDPClientHandler(reqNum, start)
 }
 
-func UDPClientHandler(conn net.PacketConn, reqNum int, start time.Time) {
-
+func UDPClientHandler(reqNum int, start time.Time) {
+	serverPort := "20532"
+	pconn, _ := net.ListenPacket("udp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
 	typeBuffer := make([]byte, 1024)
 	buffer := make([]byte, 1024)
 
 	for {
 		//pconn, _ := net.ListenPacket("udp", ":"+serverPort)
 		//fmt.Printf("Server is ready to receive on port %s\n", serverPort)
-		count, r_addr, _ := conn.ReadFrom(typeBuffer)
+		count, r_addr, _ := pconn.ReadFrom(typeBuffer)
 		if count == 0 {
 			return
 		}
@@ -51,21 +48,21 @@ func UDPClientHandler(conn net.PacketConn, reqNum int, start time.Time) {
 		fmt.Printf("Command %s\n", typeStr)
 
 		if typeStr == "1" {
-			t, in_addr, _ := conn.ReadFrom(buffer)
-			conn.WriteTo(bytes.ToUpper(buffer[:t]), in_addr)
+			t, in_addr, _ := pconn.ReadFrom(buffer)
+			pconn.WriteTo(bytes.ToUpper(buffer[:t]), in_addr)
 		} else if typeStr == "2" {
-			conn.WriteTo([]byte(r_addr.String()), r_addr)
+			pconn.WriteTo([]byte(r_addr.String()), r_addr)
 		} else if typeStr == "3" {
-			conn.WriteTo([]byte(strconv.Itoa(reqNum)), r_addr)
+			pconn.WriteTo([]byte(strconv.Itoa(reqNum)), r_addr)
 		} else if typeStr == "4" {
 			duration := time.Since(start)
 			hour := int(duration.Seconds() / 3600)
 			minute := int(duration.Seconds()/60) % 60
 			second := int(duration.Seconds()) % 60
 			totalRuntime := fmt.Sprintf("%02d:%02d:%02d\n", hour, minute, second)
-			conn.WriteTo([]byte(totalRuntime), r_addr)
+			pconn.WriteTo([]byte(totalRuntime), r_addr)
 		} else if typeStr == "5" {
-			conn.WriteTo([]byte("-1"), r_addr)
+			pconn.WriteTo([]byte("-1"), r_addr)
 		}
 		reqNum++
 	}
