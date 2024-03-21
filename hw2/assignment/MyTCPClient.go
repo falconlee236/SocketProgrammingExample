517049a90f5caa41e43a5cc537a60584bcedcb73
falconlee236 <falconlee236@cau.ac.kr>
2024-03-21 09:49:07 +0900

    add for statement in client

--- hw2/assignment/MyTCPClient.go
+++ hw2/assignment/MyTCPClient.go
@@ -35,39 +35,41 @@ func main() {
 
 	localAddr := conn.LocalAddr().(*net.TCPAddr)
 	fmt.Printf("Client is running on port %d\n", localAddr.Port)
+	defer conn.Close()
 
-	fmt.Printf("<Menu>\n")
-	fmt.Printf("1) convert text to UPPER-case\n")
-	fmt.Printf("2) get my IP address and port number\n")
-	fmt.Printf("3) get server request count\n")
-	fmt.Printf("4) get server running time\n")
-	fmt.Printf("5) exit\n")
-	fmt.Printf("Input option: ")
-	input_option, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	start := time.Now()
-	conn.Write([]byte(input_option))
+	for {
+		fmt.Printf("<Menu>\n")
+		fmt.Printf("1) convert text to UPPER-case\n")
+		fmt.Printf("2) get my IP address and port number\n")
+		fmt.Printf("3) get server request count\n")
+		fmt.Printf("4) get server running time\n")
+		fmt.Printf("5) exit\n")
+		fmt.Printf("Input option: ")
+		input_option, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		start := time.Now()
+		conn.Write([]byte(input_option))
 
-	if strings.TrimRight(input_option, "\n") == "1" {
-		fmt.Printf("Input lowercase sentence: ")
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		start = time.Now()
-		conn.Write([]byte(input))
+		if strings.TrimRight(input_option, "\n") == "1" {
+			fmt.Printf("Input lowercase sentence: ")
+			input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+			start = time.Now()
+			conn.Write([]byte(input))
+		}
+		buffer := make([]byte, 1024)
+		conn.Read(buffer)
+		duration := time.Since(start)
+		if strings.TrimRight(input_option, "\n") == "1" {
+			fmt.Printf("\nReply from server: %s", string(buffer))
+		} else if strings.TrimRight(input_option, "\n") == "2" {
+			recInfo := strings.Split(string(buffer), ":")
+			fmt.Printf("\nReply from server: client IP = %s PORT = %s\n", recInfo[0], recInfo[1])
+		} else if strings.TrimRight(input_option, "\n") == "3" {
+			fmt.Printf("\nReply from server: requests served = %s\n", string(buffer))
+		} else if strings.TrimRight(input_option, "\n") == "4" {
+			fmt.Printf("\nReply from server: run time = %s", string(buffer))
+		} else if strings.TrimRight(input_option, "\n") == "5" {
+			fmt.Print("Bye bye~\n")
+		}
+		fmt.Printf("RTT = %s\n", duration)
 	}
-	buffer := make([]byte, 1024)
-	conn.Read(buffer)
-	duration := time.Since(start)
-	if strings.TrimRight(input_option, "\n") == "1" {
-		fmt.Printf("\nReply from server: %s", string(buffer))
-	} else if strings.TrimRight(input_option, "\n") == "2" {
-		recInfo := strings.Split(string(buffer), ":")
-		fmt.Printf("\nReply from server: client IP = %s PORT = %s\n", recInfo[0], recInfo[1])
-	} else if strings.TrimRight(input_option, "\n") == "3" {
-		fmt.Printf("\nReply from server: requests served = %s\n", string(buffer))
-	} else if strings.TrimRight(input_option, "\n") == "4" {
-		fmt.Printf("\nReply from server: run time = %s", string(buffer))
-	} else if strings.TrimRight(input_option, "\n") == "5" {
-		fmt.Print("Bye bye~\n")
-	}
-	fmt.Printf("RTT = %s\n", duration)
-	conn.Close()
 }
