88d97dd4b303112b017a4750edb4756fa4e797ac
falconlee236 <falconlee236@cau.ac.kr>
2024-03-24 14:39:18 +0900

    init UDP java

--- hw2/bouns_assignment/EasyUDPClient.java
+++ hw2/bouns_assignment/EasyUDPClient.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class EasyUDPClient {
+import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EasyUDPClient {
+    public static void main(String[] args) {
+        final String SERVER_ADDRESS = "127.0.0.1";
+        final int PORT = 30532;
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("\nBye bye~");
+        }));
+
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
+            InetAddress serverAddress = InetAddress.getByName(SERVER_ADDRESS);
+            Scanner sc = new Scanner(System.in);
+            System.out.printf("The client is running on port %d\n", clientSocket.getLocalPort());
+
+            while (true) {
+                System.out.println("<Menu>");
+                System.out.println("1) convert text to UPPER-case");
+                System.out.println("2) get my IP address and port number");
+                System.out.println("3) get server request count");
+                System.out.println("4) get server running time");
+                System.out.println("5) exit");
+                System.out.print("Input option: ");
+                String strType = sc.nextLine().replaceFirst("\n", "");;
+                long startTime = System.nanoTime();
+                byte [] strTypeBytes = strType.getBytes();
+
+                DatagramPacket sendStrTypePacket =
+                        new DatagramPacket(strTypeBytes, strTypeBytes.length, serverAddress, 30532);
+                clientSocket.send(sendStrTypePacket);
+
+                if (strType.equals("1")){
+                    System.out.print("Input sentence: ");
+                    String text = sc.nextLine().replaceFirst("\n", "");
+                    startTime = System.nanoTime();
+
+                    byte[] textBytes = text.getBytes();
+                    DatagramPacket sendTextPacket =
+                            new DatagramPacket(textBytes, textBytes.length, serverAddress, PORT);
+                    clientSocket.send(sendTextPacket);
+                } else if (strType.equals("5")){
+                    System.out.println("Bye bye~");
+                    throw new InterruptedException();
+                }
+
+                byte[] responseBytes = new byte[4096];
+                DatagramPacket receiveResponsePacket =
+                        new DatagramPacket(responseBytes, responseBytes.length);
+                clientSocket.receive(receiveResponsePacket);
+
+                String response = new String(receiveResponsePacket.getData());
+                long endTime = System.nanoTime();
+                System.out.println("Reply from server: " + response);
+                System.out.printf("RTT = %fms\n", (endTime - startTime) / 1e+6);
+                Thread.sleep(1000); // Optional delay
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            if (e.getClass().getName().equals("java.net.SocketException")){
+                System.out.println("Server Disconnected");
+            }
+        }
+    }
 }
--- hw2/bouns_assignment/EasyUDPServer.java
+++ hw2/bouns_assignment/EasyUDPServer.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class EasyUDPServer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class EasyUDPServer {
+    public static void main(String[] args) {
+        LocalTime serverStartTime = LocalTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        final int PORT = 30532;
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("\nBye bye~");
+        }));
+        try (DatagramSocket serverSocket = new DatagramSocket(PORT)){
+            while (true){
+                System.out.printf("The server is ready to receive on port %d\n", PORT);
+
+                byte[] inputLineBytes = new byte[1000];
+                DatagramPacket receiveInputLinePacket = new DatagramPacket(inputLineBytes, inputLineBytes.length);
+                serverSocket.receive(receiveInputLinePacket);
+
+                String inputLine = new String(receiveInputLinePacket.getData());
+                int reqNum = 0;
+                System.out.println("Command " + inputLine);
+
+                String result = "";
+                if (inputLine.equals("1")){
+                    byte[] textBytes = new byte[2048];
+                    DatagramPacket receiveTextBytesPacket = new DatagramPacket(textBytes, textBytes.length);
+                    serverSocket.receive(receiveTextBytesPacket);
+
+                    result = new String(receiveTextBytesPacket.getData()).toUpperCase();
+                } else if (inputLine.equals("2")){
+                    result = String.format("client IP = %s, port = %d",
+                            receiveInputLinePacket.getAddress().getHostAddress(),
+                            receiveInputLinePacket.getPort());
+                } else if (inputLine.equals("3")){
+                    result = String.format("requests served = %d", reqNum);
+                } else if (inputLine.equals("4")){
+                    LocalTime currentServerTime = LocalTime.now();
+                    Duration duration = Duration.between(serverStartTime, currentServerTime);
+                    result = String.format("run time = %s", formatDuration(duration));
+                }
+
+                byte[] resultBytes = new byte[4096];
+                InetAddress clientAddress = receiveInputLinePacket.getAddress();
+                int clientPort = receiveInputLinePacket.getPort();
+                DatagramPacket sendResultPacket =
+                        new DatagramPacket(resultBytes, resultBytes.length, clientAddress, clientPort);
+                serverSocket.send(sendResultPacket);
+                reqNum++;
+            }
+        } catch (IOException e) {
+//            e.printStackTrace();
+        }
+    }
+
+    private static String formatDuration(Duration duration) {
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes() % 60;
+        long seconds = duration.getSeconds() % 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
 }
