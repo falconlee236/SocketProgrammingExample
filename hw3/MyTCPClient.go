b039189555f9d8e4af19341df406f2301d29eded
falconlee236 <falconlee236@cau.ac.kr>
2024-04-01 12:27:43 +0900

    start hw3

---
+++ hw3/MyTCPClient.go
@@ -0,0 +1,80 @@
+/*
+ * MyTCPClient.go
+20190532 sangyunLee
+ **/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			fmt.Print("\nBye bye~\n")
+			os.Exit(1)
+		}
+	}()
+
+	serverName := "127.0.0.1"
+	serverPort := "20532"
+
+	conn, err := net.Dial("tcp", serverName+":"+serverPort)
+	if err != nil {
+		log.Fatalf("Failed to connect to server: %v", err)
+		return
+	}
+
+	localAddr := conn.LocalAddr().(*net.TCPAddr)
+	fmt.Printf("Client is running on port %d\n", localAddr.Port)
+	defer conn.Close()
+
+	for {
+		fmt.Printf("<Menu>\n")
+		fmt.Printf("1) convert text to UPPER-case\n")
+		fmt.Printf("2) get my IP address and port number\n")
+		fmt.Printf("3) get server request count\n")
+		fmt.Printf("4) get server running time\n")
+		fmt.Printf("5) exit\n")
+		fmt.Printf("Input option: ")
+		inputOption, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		inputNum, _ := strconv.Atoi(strings.TrimRight(inputOption, "\n"))
+		if inputNum < 1 || inputNum > 5 {
+			fmt.Print("Invalid option\n")
+			continue
+		}
+		start := time.Now()
+		conn.Write([]byte(inputOption))
+
+		if strings.TrimRight(inputOption, "\n") == "1" {
+			fmt.Printf("Input lowercase sentence: ")
+			input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+			start = time.Now()
+			conn.Write([]byte(input))
+		} else if strings.TrimRight(inputOption, "\n") == "5" {
+			fmt.Printf("Bye bye~")
+			return
+		}
+		buffer := make([]byte, 1024)
+		read, err := conn.Read(buffer)
+		duration := time.Since(start)
+		if err != nil || read == 0 {
+			log.Fatalf("Failed to connect to server: %v", err)
+			return
+		}
+		fmt.Printf("\nReply from server: %s", string(buffer))
+		fmt.Printf("RTT = %fms\n", float64(duration.Nanoseconds())/1e+6)
+	}
+}
---
+++ hw3/MyTCPServer.go
@@ -0,0 +1,81 @@
+/**
+ * MyTCPServer.go
+20190532 sangyunLee
+ **/
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+)
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			fmt.Print("\nBye bye~\n")
+			os.Exit(1)
+		}
+	}()
+
+	start := time.Now()
+	reqNum := 0
+	serverPort := "20532"
+
+	listener, _ := net.Listen("tcp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		go TCPClientHandler(conn, reqNum, start)
+	}
+}
+
+func TCPClientHandler(conn net.Conn, reqNum int, start time.Time) {
+	defer conn.Close()
+
+	typeBuffer := make([]byte, 1024)
+
+	for {
+		buffer := make([]byte, 1024)
+		t, _ := conn.Read(typeBuffer)
+		if t == 0 {
+			return
+		}
+		typeStr := string(typeBuffer[:t-1])
+		fmt.Printf("Command %s\n", typeStr)
+
+		var result string = ""
+		if typeStr == "1" {
+			count, _ := conn.Read(buffer)
+			result = string(bytes.ToUpper(buffer[:count]))
+		} else if typeStr == "2" {
+			addrs := strings.Split(conn.RemoteAddr().String(), ":")
+			result = fmt.Sprintf("client IP = %s, port = %s\n", addrs[0], addrs[1])
+		} else if typeStr == "3" {
+			result = fmt.Sprintf("request served = %d\n", reqNum)
+		} else if typeStr == "4" {
+			duration := time.Since(start)
+			hour := int(duration.Seconds() / 3600)
+			minute := int(duration.Seconds()/60) % 60
+			second := int(duration.Seconds()) % 60
+			result = fmt.Sprintf("run time = %02d:%02d:%02d\n", hour, minute, second)
+		}
+		conn.Write([]byte(result))
+		reqNum++
+	}
+}
