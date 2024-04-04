aa07b1db3455937cecb73b9d2cd1d0efbe902b49
falconlee236 <falconlee236@cau.ac.kr>
2024-04-04 17:43:24 +0900

    remove UDP Server code

--- hw2/assignment/MyUDPServer.go
+++ hw2/assignment/MyUDPServer.go
@@ -11,7 +11,6 @@ import (
 	"net"
 	"os"
 	"os/signal"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -60,45 +59,6 @@ func main() {
 			result = fmt.Sprintf("run time = %02d:%02d:%02d\n", hour, minute, second)
 		}
 		pconn.WriteTo([]byte(result), r_addr)
-		reqNum++
-	}
-	//go UDPClientHandler(reqNum, start)
-}
-
-func UDPClientHandler(reqNum int, start time.Time) {
-	serverPort := "20532"
-	pconn, _ := net.ListenPacket("udp", ":"+serverPort)
-	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
-	typeBuffer := make([]byte, 1024)
-	buffer := make([]byte, 1024)
-
-	for {
-		//pconn, _ := net.ListenPacket("udp", ":"+serverPort)
-		//fmt.Printf("Server is ready to receive on port %s\n", serverPort)
-		count, r_addr, _ := pconn.ReadFrom(typeBuffer)
-		if count == 0 {
-			return
-		}
-		typeStr := string(typeBuffer[:count-1])
-		fmt.Printf("Command %s\n", typeStr)
-
-		if typeStr == "1" {
-			t, in_addr, _ := pconn.ReadFrom(buffer)
-			pconn.WriteTo(bytes.ToUpper(buffer[:t]), in_addr)
-		} else if typeStr == "2" {
-			pconn.WriteTo([]byte(r_addr.String()), r_addr)
-		} else if typeStr == "3" {
-			pconn.WriteTo([]byte(strconv.Itoa(reqNum)), r_addr)
-		} else if typeStr == "4" {
-			duration := time.Since(start)
-			hour := int(duration.Seconds() / 3600)
-			minute := int(duration.Seconds()/60) % 60
-			second := int(duration.Seconds()) % 60
-			totalRuntime := fmt.Sprintf("%02d:%02d:%02d\n", hour, minute, second)
-			pconn.WriteTo([]byte(totalRuntime), r_addr)
-		} else if typeStr == "5" {
-			pconn.WriteTo([]byte("-1"), r_addr)
-		}
 		reqNum++
 	}
 }
