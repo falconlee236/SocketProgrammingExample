33afc5ccf3685be0f0d90454986c883ca04c2603
falconlee236 <falconlee236@cau.ac.kr>
2024-05-08 03:40:38 +0900

    start base code

--- hw5/SplitFileClient.go
+++ hw5/SplitFileClient.go
@@ -4,6 +4,125 @@ SplitFileClient.go
 */
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
 
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Invalid argument.")
+		os.Exit(1)
+	}
+	//commandName := os.Args[1]
+	fileName := os.Args[2]
+	// 서버의 주소와 포트
+	serverName := "127.0.0.1"
+	serverPort := "20532"
+
+	// 파일을 두 부분으로 나눔
+	part1, part2, err := splitFile(fileName)
+	if err != nil {
+		fmt.Println("파일 분할 실패:", err)
+		return
+	}
+
+	// TCP 서버에 연결
+	conn, err := net.Dial("tcp", serverName+":"+serverPort)
+	if err != nil {
+		fmt.Println("서버에 연결 실패:", err)
+		return
+	}
+	defer conn.Close()
+
+	// 첫 번째 부분 파일 전송
+	sendFile(conn, part1, fileName+"-part1.txt")
+
+	// 두 번째 부분 파일 전송
+	sendFile(conn, part2, fileName+"-part2.txt")
+
+	fmt.Println("파일 전송 완료")
+}
+
+// 파일을 TCP 소켓을 통해 서버로 전송하는 함수
+func sendFile(conn net.Conn, file *os.File, filename string) {
+	// 파일 정보 가져오기
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println("파일 정보 가져오기 실패:", err)
+		return
+	}
+
+	// 파일 크기 전송
+	fileSize := make([]byte, 1024)
+	size := strconv.FormatInt(fileInfo.Size(), 10)
+	copy(fileSize[:], size)
+	conn.Write(fileSize)
+
+	// 파일 이름 전송
+	conn.Write([]byte(filename))
+
+	// 파일 내용 전송
+	bufferSize := 1024
+	buffer := make([]byte, bufferSize)
+	for {
+		bytesRead, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		conn.Write(buffer[:bytesRead])
+	}
+}
+
+// 파일을 두 개의 부분으로 분할하는 함수
+func splitFile(filename string) (part1, part2 *os.File, err error) {
+	// 원본 파일 열기
+	originalFile, err := os.Open(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// 부분 파일들 생성
+	part1, err = os.Create(filename + "-part1.txt")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	part2, err = os.Create(filename + "-part2.txt")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// 원본 파일을 읽기 위한 버퍼 생성
+	reader := bufio.NewReader(originalFile)
+
+	// 바이트 단위로 파일을 읽고, 홀수와 짝수 바이트를 번갈아가며 각각의 파일에 쓰기
+	var byteCount int
+	for {
+		// 파일에서 한 바이트 읽기
+		b, err := reader.ReadByte()
+		if err != nil {
+			break // 파일 끝에 도달하면 종료
+		}
+
+		// 홀수 바이트는 part1에, 짝수 바이트는 part2에 쓰기
+		if byteCount%2 == 0 {
+			_, err := part1.Write([]byte{b}) // part1에 바이트 쓰기
+			if err != nil {
+				return nil, nil, err
+			}
+		} else {
+			_, err := part2.Write([]byte{b}) // part2에 바이트 쓰기
+			if err != nil {
+				return nil, nil, err
+			}
+		}
+
+		byteCount++
+	}
+
+	return part1, part2, nil
 }
--- hw5/SplitFileServer.go
+++ hw5/SplitFileServer.go
@@ -1,9 +1,99 @@
-/*
-SplitFileServer.go
-20190532 sang yun lee
-*/
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+)
 
+func main() {
+	serverPort := "20532"
+
+	// 서버 시작
+	listener, err := net.Listen("tcp", ":"+serverPort)
+	if err != nil {
+		fmt.Println("서버 시작 실패:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("서버 시작, 클라이언트 연결 대기중...")
+
+	for {
+		// 클라이언트 연결 대기
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("클라이언트 연결 실패:", err)
+			continue
+		}
+
+		// 클라이언트가 연결되었을 때 파일을 수신하는 함수 호출
+		go handleClient(conn)
+	}
+}
+
+// 클라이언트로부터 파일을 수신하여 저장하는 함수
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	// 파일 크기 읽기
+	fileSizeBuffer := make([]byte, 8)
+	cnt, err := conn.Read(fileSizeBuffer)
+	if err != nil {
+		fmt.Println("파일 크기 읽기 실패:", err)
+		return
+	}
+
+	// 파일 이름 읽기
+	filenameBuffer := make([]byte, 1024)
+	_, err = conn.Read(filenameBuffer)
+	if err != nil {
+		fmt.Println("파일 이름 읽기 실패:", err)
+		return
+	}
+	filename := string(filenameBuffer)
+	fmt.Println(filename)
+
+	// 파일 크기 변환
+	fileSize, err := strconv.ParseInt(string(fileSizeBuffer[:cnt]), 10, 64)
+	if err != nil {
+		fmt.Println("파일 크기 변환 실패:", err)
+		return
+	}
+
+	// 파일 생성
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("파일 생성 실패:", err)
+		return
+	}
+	defer file.Close()
+
+	// 파일 내용 수신하여 저장
+	var receivedBytes int64
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("파일 읽기 실패:", err)
+			}
+			break
+		}
+		receivedBytes += int64(n)
+
+		// 파일에 받은 내용 쓰기
+		if _, err := file.Write(buffer[:n]); err != nil {
+			fmt.Println("파일 쓰기 실패:", err)
+			return
+		}
+
+		// 파일이 전부 받아졌는지 확인
+		if receivedBytes >= fileSize {
+			break
+		}
+	}
+
+	fmt.Printf("%s 파일 전송 완료\n", filename)
 }
