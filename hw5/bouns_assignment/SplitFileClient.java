b3f6bd52bcb6ff4074c185eb189e1d8998885a03
falconlee236 <falconlee236@cau.ac.kr>
2024-05-12 23:10:11 +0900

    start java

---
+++ hw5/bouns_assignment/SplitFileClient.java
@@ -0,0 +1,10 @@
+/*
+SplitFileClient.java
+20190532 sang yun lee
+*/
+
+class SplitFileClient {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+}
---
+++ hw5/bouns_assignment/SplitFileServer.java
@@ -0,0 +1,10 @@
+/*
+SplitFileServer.java
+20190532 sang yun lee
+*/
+
+class SplitFileServer {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+}
