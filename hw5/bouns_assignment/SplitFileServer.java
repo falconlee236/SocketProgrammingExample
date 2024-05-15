f7c7b4ae9da67d1f80bf0d6a9246c76250024b1d
falconlee236 <falconlee236@cau.ac.kr>
2024-05-15 14:40:41 +0900

    start java

--- hw5/bouns_assignment/SplitFileServer.java
+++ hw5/bouns_assignment/SplitFileServer.java
@@ -4,7 +4,20 @@ SplitFileServer.java
 */
 
 class SplitFileServer {
-	public static void main(String[] args) {
-		System.out.println("hello world!");
+	public static void main(String[] args){
+		// argument handling
+		if (args.length != 3){
+			System.out.println("Invalid argument.");
+			System.exit(1);
+		}
+		// get argument from system args
+		String commandName = args[1];
+		String fileName = args[2];
+
+		// server Info hardcoding
+		String firstServerName = "127.0.0.1";
+		String firstServerPort = "40532";
+		String secondServerName = "127.0.0.1";
+		String secondServerPort = "50532";
 	}
 }
