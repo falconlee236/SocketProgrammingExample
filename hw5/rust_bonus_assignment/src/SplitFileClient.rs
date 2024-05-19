8719cd3b22037eef10e44d1340fd76a9b4981a34
falconlee236 <falconlee236@cau.ac.kr>
2024-05-19 22:00:01 +0900

    start rust project

---
+++ hw5/rust_bonus_assignment/src/SplitFileClient.rs
@@ -0,0 +1,8 @@
+/*
+SplitFileClient.rs
+20190532 sang yun lee
+*/
+
+fn main() {
+	println!("hello world!");
+}
---
+++ hw5/rust_bonus_assignment/src/SplitFileServer.rs
@@ -0,0 +1,8 @@
+/*
+SplitFileServer.rs
+20190532 sang yun lee
+*/
+
+fn main() {
+	println!("hello world!");
+}
