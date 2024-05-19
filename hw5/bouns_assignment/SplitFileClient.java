11cc3f659262aab0291a53d69de057ea904b6087
falconlee236 <falconlee236@cau.ac.kr>
2024-05-19 17:00:07 +0900

    add recieve feature in client.java

--- hw5/bouns_assignment/SplitFileClient.java
+++ hw5/bouns_assignment/SplitFileClient.java
@@ -6,7 +6,9 @@ SplitFileClient.java
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -60,6 +62,32 @@ class SplitFileClient {
                 Thread.currentThread().interrupt();
                 System.out.println("Thread interrupted");
             }
+		}// get command case
+		else if (commandName.equals("get")){
+			Runnable task1 = () -> {
+				try {
+					recieveFile(fileName, firstServerName, firstServerPort, 0);		
+				} finally {
+					latch.countDown();
+				}
+			};
+			Runnable task2 = () -> {
+				try {
+					recieveFile(fileName, secondServerName, secondServerPort, 1);
+				} finally {
+					latch.countDown();
+				}
+			};
+			executor.submit(task1);
+			executor.submit(task2);
+			// Wait for all tasks to complete
+            try {
+                latch.await();  // Wait until the count reaches 0
+                executor.shutdown();  // Shutdown the executor
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Thread interrupted");
+            }
 		}
 	}
 
@@ -76,7 +104,6 @@ class SplitFileClient {
 			out.println(commandStr);
 			// get from server
 			String commandRes = in.readLine();
-			System.out.println(commandRes);
 			if (!commandRes.equals("ok")){
 				System.out.println("fail to receive fileName");
 				throw new IOException();
@@ -117,6 +144,59 @@ class SplitFileClient {
 					byteCount++;
 				}
 				System.out.println(fileName + " send successful");
+			} catch (Exception e) {
+				System.out.println("File Error");
+				System.exit(1);
+			}
+
+		} catch(IOException e){
+			System.out.println("fail to connect server");
+			System.exit(1);
+		}
+	}
+
+	private static void recieveFile(String fileName, String serverName, String serverPort, int part){
+		System.out.println("Request to server to get : " + fileName);
+		try (
+			Socket socket = new Socket(serverName, Integer.parseInt(serverPort));
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			InputStream is = socket.getInputStream();
+		) {
+			String commandStr = "get";
+			// send to server
+			out.println(commandStr);
+			// get from server
+			String commandRes = in.readLine();
+			if (!commandRes.equals("ok")){
+				System.out.println("fail to receive fileName");
+				throw new IOException();
+			}
+			
+			int idx = fileName.lastIndexOf('.');
+			String fileExtension = fileName.substring(idx+1);
+			fileName = fileName.substring(0, idx);
+			fileName = String.format("%s-part%d.%s", fileName, part + 1, fileExtension);
+			out.println(fileName);
+			// get file Size from server
+			String fileSizeBuffer = in.readLine();
+			long fileSize = Long.parseLong(fileSizeBuffer);
+			out.println("ok");
+			
+			
+			// get file Object
+			// file create = 제출할때는 tmp 빼야함.
+			// File tmpFile = new File(fileName);
+			File tmpFile = new File(fileName + "tmp" + fileExtension);
+			try (FileOutputStream fos = new FileOutputStream(tmpFile)){
+				long receivedBytes = 0;
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				while (receivedBytes < fileSize && (bytesRead = is.read(buffer)) != -1) { 
+					fos.write(buffer, 0, bytesRead);
+					receivedBytes += bytesRead;	
+				}
+				System.out.println(fileName + " store successful");
 			} catch (Exception e) {
 				System.out.println("File Error");
 				System.exit(1);
