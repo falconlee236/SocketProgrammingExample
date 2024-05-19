8ac63c7f663275628d843853f29d6687b351b03e
falconlee236 <falconlee236@cau.ac.kr>
2024-05-19 16:18:33 +0900

    add InputStream and OutputStream class in files

--- hw5/bouns_assignment/SplitFileClient.java
+++ hw5/bouns_assignment/SplitFileClient.java
@@ -8,20 +8,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Arrays;
 
 class SplitFileClient {
 	public static void main(String[] args) {
 		// argument handling
-		if (args.length != 3){
+		if (args.length != 2){
 			System.out.println("Invalid argument.");
 			System.exit(1);
 		}
 		// get argument from system args
-		String commandName = args[1];
-		String fileName = args[2];
+		String commandName = args[0];
+		String fileName = args[1];
 
 		// server Info hardcoding
 		String firstServerName = "127.0.0.1";
@@ -41,13 +41,15 @@ class SplitFileClient {
 		try (
 			Socket socket = new Socket(serverName, Integer.parseInt(serverPort));
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			OutputStream os = socket.getOutputStream();
 		) {
 			String commandStr = "put";
 			// send to server
-			out.print(commandStr);
+			out.println(commandStr);
 			// get from server
 			String commandRes = in.readLine();
+			System.out.println(commandRes);
 			if (!commandRes.equals("ok")){
 				System.out.println("fail to receive fileName");
 				throw new IOException();
@@ -59,8 +61,8 @@ class SplitFileClient {
 			int idx = fileName.lastIndexOf('.');
 			String fileExtension = fileName.substring(idx+1);
 			fileName = fileName.substring(0, idx);
-			fileName = String.format("%s-part%d%s", fileName, part + 1, fileExtension);
-			out.print(fileName);
+			fileName = String.format("%s-part%d.%s", fileName, part + 1, fileExtension);
+			out.println(fileName);
 			// get from server
 			String fileNameRes = in.readLine();
 			if (!fileNameRes.equals("ok")){
@@ -70,7 +72,7 @@ class SplitFileClient {
 
 			if (srcFile.exists() && srcFile.isFile()){
 				long size = srcFile.length();
-				out.print(Long.toString(size));
+				out.println(Long.toString(size));
 				String fileSizeRes = in.readLine();
 				if (!fileSizeRes.equals("ok")){
 					System.out.println("fail to receive fileSize");
@@ -83,7 +85,7 @@ class SplitFileClient {
 				byte[] b = new byte[1];
 				while (fis.read(b) > 0){
 					if (byteCount % 2 == part){
-						out.print(Arrays.toString(b));
+						os.write(b);
 					}
 					byteCount++;
 				}
--- hw5/bouns_assignment/SplitFileServer.java
+++ hw5/bouns_assignment/SplitFileServer.java
@@ -1,6 +1,7 @@
 
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -14,48 +15,53 @@ SplitFileServer.java
 class SplitFileServer {
 	public static void main(String[] args){
 		// argument handling
-		if (args.length != 2){
+		if (args.length != 1){
 			System.out.println("Invalid argument.");
 			System.exit(1);
 		}
 		// server Port
-		String serverPort = args[1];
+		String serverPort = args[0];
 		try (
 			ServerSocket serverSocket = new ServerSocket(Integer.parseInt(serverPort))
 		){
+			System.out.println("wait for client request");
 			while (true) { 
 				Socket clientSocket = serverSocket.accept();
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				InputStream is = clientSocket.getInputStream();
 
 				// get command from client
 				String commandName = in.readLine();
-				out.print("ok");
+				out.println("ok");
 
 				if (commandName.equals("put")){
 					String fileName = in.readLine();
-					out.print("ok");
+					out.println("ok");
 					System.out.println("Received file: " + fileName);
 
 					String fileSizeBuffer = in.readLine();
-					out.print("ok");
+					out.println("ok");
 					int fileSize = Integer.parseInt(fileSizeBuffer);
 					try (
 						FileOutputStream fos = new FileOutputStream(fileName)
 					){
 						long receivedBytes = 0;
-						while (receivedBytes >= fileSize) { 
-							String n = in.readLine();
-							receivedBytes += n.length();	
-							fos.write(n.getBytes());
+						byte[] buffer = new byte[1024];
+						int bytesRead;
+						while (receivedBytes < fileSize && (bytesRead = is.read(buffer)) != -1) { 
+							fos.write(buffer, 0, bytesRead);
+							receivedBytes += bytesRead;	
 						}
 					} catch (Exception e) {
 						System.out.println("fail to create file");
-						throw new Exception();
+						continue;
 					}
 					System.out.println(fileName + " file store sucessful!");
 				}
 			}
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			System.out.println("Error!");
+		}
 	}
 }
